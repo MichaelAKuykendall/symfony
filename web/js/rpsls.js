d420ebaf5ba8820eae9bd35364d092f438a3c1dd
Mike Kuykendall <mike.kuykendall@golocalinteractive.com>
2016-03-11 16:43:39 -0600

    Working the JS logic for winning anf setting cookies

--- web/js/rpsls.js
+++ web/js/rpsls.js
@@ -6,24 +6,88 @@ var options = ["paper", "rock", "lizard", "spock", "scissors"],
 
     randomChoice = options[i],
 
-    doIt = function(choice1, choice2) {
+    doIt = function (choice1, choice2) {
         var index1 = options.indexOf(choice1),
             index2 = options.indexOf(choice2),
             dif = index2 - index1;
-        if(dif < 0) {
+        if (dif < 0) {
             dif += options.length;
         }
-        while(dif > 2) {
+        while (dif > 2) {
             dif -= 2;
         }
-         //return choice1 + result[dif] + choice2;
-        document.getElementById(choice1).style.borderColor="#FF0000";
-        document.getElementById(choice2).style.borderColor="#FF0000";
+        //return choice1 + result[dif] + choice2;
 
-        document.cookie = 'cookie1=test; expires=Fri, 3 Aug 2001 20:47:11 UTC; path=/';
+        document.getElementById(choice1).style.borderColor = "#FF0000";
+        document.getElementById(choice2).style.borderColor = "#FF0000";
 
-        console.log(choice1 + result[dif] + choice2);
-        // /alert(choice1 + result[dif] + choice2);
 
-        alert("Your Cookie : " + document.cookie);
+        //console.log(choice1 + result[dif] + choice2);
+
+        //alert(choice1 + result[dif] + choice2);
+
+        function getCookie(name) {
+            var value = "; " + document.cookie;
+            var parts = value.split("; " + name + "=");
+            if (parts.length == 2) return parts.pop().split(";").shift();
+        }
+
+        console.log(getCookie("playerwon"));
+
+        if (document.cookie.indexOf("playerwon") >= 0) {
+            var pw = new RegExp("playerwon" + "=([^;]+)");
+            var playerwon = pw.exec(document.cookie);
+            playerwon = parseInt(playerwon);
+            console.log(playerwon);
+        }
+
+        if (document.cookie.indexOf("playertied") >= 0) {
+            var pt = new RegExp("playertied" + "=([^;]+)");
+            var playertied = pw.exec(document.cookie);
+            playertied = parseInt(playertied);
+            console.log(playertied);
+        }
+
+        if (document.cookie.indexOf("playertied") >= 0) {
+            var pl = new RegExp("playerlost" + "=([^;]+)");
+            var playerlost = pw.exec(document.cookie);
+            playerlost = parseInt(playerlost);
+            console.log(playerlost);
+        }
+
+        switch (dif) {
+            case 0:
+                if(playertied != null) {
+                    playertied++;
+                } else {
+                    playertied = "1";
+                }
+
+                document.cookie = "playertied=" + playertied;
+                break;
+
+            case 1:
+                if(playerwon != null) {
+                    playerwon++;
+                } else {
+                    playerwon = "1";
+                }
+                document.cookie = "playerwon=" + playerwon;
+                break;
+
+            case 2:
+                if(playerlost != null) {
+                    playerlost++;
+                } else {
+                    playerlost = "1";
+                }
+                document.cookie = "playerlost=" + playerlost;
+                break;
+        }
+
+        //var cookie = document.cookie;
+        //var cstart = cookie.indexOf("playerwon=");
+        //console.log(getCookie());
+
+        //alert("Your Cookie : " + document.cookie);
     };
